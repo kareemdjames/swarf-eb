60ee29cd0d094793b53f0e003a78ff1f92297bf2
Kareem James <kareemdjames@gmail.com>
2018-08-05 23:26:56 -0400

    added react-bootstrap

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Button } from 'react-bootstrap';
 import './App.css'
 
 class App extends Component {
@@ -10,6 +11,8 @@ class App extends Component {
     return (
       <div className="app">
         <h1>Welcome from React</h1>
+        <Button bsStyle="primary">Primary</Button>
+        <Button bsStyle="danger">Danger</Button>
       </div>  
     );
   }
