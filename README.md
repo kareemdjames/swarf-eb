1f7fc7b902b73706da4fbc31d6e0385e47110cc9
Kareem James <kareemdjames@gmail.com>
2018-08-05 21:25:50 -0400

    initial setup of react project

---
+++ README.md
@@ -0,0 +1,1 @@
+# SWARF-EB
---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import './App.css'
+
+class App extends Component {
+  constructor() {
+    super();
+  }
+
+  render() {
+    return (
+      <div className="app">
+        <h1>Welcome from React</h1>
+      </div>  
+    );
+  }
+}
+
+export default App;
