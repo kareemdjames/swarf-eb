15ebcd60852f398a17da82dfe2a08e4c9712ce9d
Kareem James <kareemdjames@gmail.com>
2018-08-06 00:55:36 -0400

    Created Home.js

---
+++ src/pages/Home.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+import { Navbar, Nav, NavItem } from 'react-bootstrap';
+
+class Home extends Component {
+  render() {
+    return (
+      <div>
+          <Navbar inverse collapseOnSelect pullLeft>
+            <Navbar.Header>
+              <Navbar.Brand>
+                <a href="#brand">HelpDesk</a>
+              </Navbar.Brand>
+              <Navbar.Toggle />
+            </Navbar.Header>
+            <Navbar.Collapse>
+              <Nav>
+                <NavItem eventKey={1} href="#">
+                  Add Ticket
+                </NavItem>
+              </Nav>
+            </Navbar.Collapse>
+          </Navbar>
+      </div>
+    );
+  }
+}
+
+export default Home;
