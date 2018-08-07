7a7f876f07fafed9d273e2579b404b8a8bfc4d15
Kareem James <kareemdjames@gmail.com>
2018-08-06 21:03:54 -0400

    added initial route and login page

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
+import {BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './App.css'
 import Nav from './components/nav/Nav';
+import Login from './pages/Login';
 
 class App extends Component {
   constructor() {
@@ -9,9 +11,13 @@ class App extends Component {
 
   render() {
     return (
-      <div className="app">
-        <Nav />
-      </div>  
+      <Router>
+        <div>
+          <Switch>
+            <Route exact path="/" component={Login} />
+          </Switch>
+        </div>
+      </Router>  
     );
   }
 }
---
+++ src/pages/Login.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+
+class Login extends Component {
+  constructor() {
+    super();
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>This is the login page</h1>
+      </div>
+    )
+  }
+
+
+
+}
+
+export default Login;
